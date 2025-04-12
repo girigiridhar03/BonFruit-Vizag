c97b15bd006cc22a7c348ff5f69c71d10ea8e111
GIRI GIRIDHAR <girigiridhar0603@gmail.com>
2025-04-12 11:15:08 +0530

    added vidoes sliders

--- src/components/HomeComponents/HomeHeader.jsx
+++ src/components/HomeComponents/HomeHeader.jsx
@@ -1,13 +1,50 @@
-import { Button } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+import { Pagination, Autoplay } from 'swiper/modules';
+
 
 const HomeHeader = () => {
   return (
-    <div>
-      Home Header
-    </div>
-  )
-} 
+    <Box mt={['6.2rem']} w={'100%'} h={['25vh','30vh','50vh','65vh','70vh','85vh']}>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        autoplay={{
+          delay: 10200,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
+        style={{ width: '100%', height: '100%' }}
+      >
+        <SwiperSlide>
+          <video
+            src="/Videos/video1.mp4"
+            autoPlay
+            muted
+            loop
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          />
+        </SwiperSlide>
 
-export default HomeHeader
+        <SwiperSlide>
+          <video
+            src="/Videos/video2.mp4"
+            autoPlay
+            muted
+            loop
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </Box>
+  );
+};
+
+export default HomeHeader;
